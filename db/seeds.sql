3832746f2589b3e0540209a64bea9e968a33e8c0
Rich McRary <rgmcrary@gmail.com>
2017-10-09 15:28:26 -0400

    new seed file

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+
+INSERT INTO burgers (burger, devoured, createdAt)
+VALUES ("bacon cheeseburger", FALSE, CURRENT_TIMESTAMP), 
+        ("mushroom swiss burger", FALSE, CURRENT_TIMESTAMP), 
+        ("triple cheeseburger", FALSE, CURRENT_TIMESTAMP);
+
+SELECT * FROM burgers;
